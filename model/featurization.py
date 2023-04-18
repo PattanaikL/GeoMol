1616e5396e9921108049865b5eb653122b9c71a5
Xiaorui Dong <xiaorui@mit.edu>
2023-04-18 13:18:03 -0400

    Simplify featurize_mol_from_smiles

--- model/featurization.py
+++ model/featurization.py
@@ -256,101 +256,6 @@ def construct_loader(args, modes=('train', 'val')):
         return loaders
 
 
-def featurize_mol_from_smiles(smiles, dataset='qm9'):
-
-    types = dataset_types[dataset]
-
-    # filter fragments
-    if '.' in smiles:
-        return None
-
-    # filter mols rdkit can't intrinsically handle
-    mol = Chem.MolFromSmiles(smiles)
-    if mol:
-        mol = Chem.AddHs(mol)
-    else:
-        return None
-    N = mol.GetNumAtoms()
-
-    # filter out mols model can't make predictions for
-    if not mol.HasSubstructMatch(dihedral_pattern):
-        return None
-    if N < 4:
-        return None
-
-    type_idx = []
-    atomic_number = []
-    atom_features = []
-    chiral_tag = []
-    neighbor_dict = {}
-    ring = mol.GetRingInfo()
-    for i, atom in enumerate(mol.GetAtoms()):
-        type_idx.append(types[atom.GetSymbol()])
-        n_ids = [n.GetIdx() for n in atom.GetNeighbors()]
-        if len(n_ids) > 1:
-            neighbor_dict[i] = torch.tensor(n_ids)
-        chiral_tag.append(chirality[atom.GetChiralTag()])
-        atomic_number.append(atom.GetAtomicNum())
-        atom_features.extend([atom.GetAtomicNum(),
-                              1 if atom.GetIsAromatic() else 0])
-        atom_features.extend(one_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5, 6]))
-        atom_features.extend(one_k_encoding(atom.GetHybridization(), [
-            Chem.rdchem.HybridizationType.SP,
-            Chem.rdchem.HybridizationType.SP2,
-            Chem.rdchem.HybridizationType.SP3,
-            Chem.rdchem.HybridizationType.SP3D,
-            Chem.rdchem.HybridizationType.SP3D2]))
-        atom_features.extend(one_k_encoding(atom.GetImplicitValence(), [0, 1, 2, 3, 4, 5, 6]))
-        atom_features.extend(one_k_encoding(atom.GetFormalCharge(), [-1, 0, 1]))
-        atom_features.extend([int(ring.IsAtomInRingOfSize(i, 3)),
-                              int(ring.IsAtomInRingOfSize(i, 4)),
-                              int(ring.IsAtomInRingOfSize(i, 5)),
-                              int(ring.IsAtomInRingOfSize(i, 6)),
-                              int(ring.IsAtomInRingOfSize(i, 7)),
-                              int(ring.IsAtomInRingOfSize(i, 8))])
-        atom_features.extend(one_k_encoding(int(ring.NumAtomRings(i)), [0, 1, 2, 3]))
-
-    z = torch.tensor(atomic_number, dtype=torch.long)
-    chiral_tag = torch.tensor(chiral_tag, dtype=torch.float)
-
-    row, col, edge_type, bond_features = [], [], [], []
-    for bond in mol.GetBonds():
-        start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
-        row += [start, end]
-        col += [end, start]
-        edge_type += 2 * [bonds[bond.GetBondType()]]
-        bt = tuple(
-            sorted([bond.GetBeginAtom().GetAtomicNum(), bond.GetEndAtom().GetAtomicNum()])), bond.GetBondTypeAsDouble()
-        bond_features += 2 * [int(bond.IsInRing()),
-                              int(bond.GetIsConjugated()),
-                              int(bond.GetIsAromatic())]
-
-    edge_index = torch.tensor([row, col], dtype=torch.long)
-    edge_type = torch.tensor(edge_type, dtype=torch.long)
-    edge_attr = F.one_hot(edge_type, num_classes=len(bonds)).to(torch.float)
-    # bond_features = torch.tensor(bond_features, dtype=torch.float).view(len(bond_type), -1)
-
-    perm = (edge_index[0] * N + edge_index[1]).argsort()
-    edge_index = edge_index[:, perm]
-    edge_type = edge_type[perm]
-    # edge_attr = torch.cat([edge_attr[perm], bond_features], dim=-1)
-    edge_attr = edge_attr[perm]
-
-    row, col = edge_index
-    hs = (z == 1).to(torch.float)
-    num_hs = scatter(hs[row], col, dim_size=N).tolist()
-
-    x1 = F.one_hot(torch.tensor(type_idx), num_classes=len(types))
-    x2 = torch.tensor(atom_features).view(N, -1)
-    x = torch.cat([x1.to(torch.float), x2], dim=-1)
-
-    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, neighbors=neighbor_dict, chiral_tag=chiral_tag,
-                name=smiles)
-    data.edge_index_dihedral_pairs = get_dihedral_pairs(data.edge_index, data=data)
-
-    return data
-
-
 def smiles_to_mol(smiles: str,
                   check_mol: bool = True):
     """
@@ -502,3 +407,15 @@ def featurize_mol(mol,
                                             data.edge_index,
                                             data=data)
         return data
+
+
+def featurize_mol_from_smiles(smiles: str,
+                              dataset='qm9'):
+    """
+    Featurize a molecule from a SMILES string.
+    """
+    mol = smiles_to_mol(smiles, check_mol=True)
+    if mol:
+        return featurize_mol(mol,
+                             dataset=dataset,
+                             name=smiles)
